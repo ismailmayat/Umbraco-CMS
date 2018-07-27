a223aaaf88e380356f5e563dbf4070840cf685cc
Ismail Mayat <imayat@thecogworks.co.uk>
2018-07-27 11:49:54 +0100

    refactoring see we get a bit of re use

--- src/Umbraco.Tests.Selenium/PageTests/Installer/InstallerTests.cs
+++ src/Umbraco.Tests.Selenium/PageTests/Installer/InstallerTests.cs
@@ -1,41 +1,19 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
-using TestStack.Seleno.Configuration;
-using TestStack.Seleno.Configuration.WebServers;
-using Umbraco.Tests.Selenium.Helper;
 
 namespace Umbraco.Tests.Selenium.PageTests.Installer
 {
     [TestFixture]
-    public class InstallerTests
+    public class InstallerTests:SeleniumTestsBase
     {
-        private SelenoHost _host;
-        public static IisExpressWebServer WebServer;
-
-        [SetUp]
-        public void SetUp()
-        {
-            var app = new WebApplication(ProjectLocation.FromFolder(Constants.ApplicationNameForIISExpress), Config.Settings.PortNo);
-            WebServer = new IisExpressWebServer(app);
-            WebServer.Start();
-            _host = Host.Instance;
-        }
-
         [Test]
         public void When_Navigating_To_Site_Root_Expect_Installer_Page()
         {
             //todo as part of setup we need to ensure no db is setup so that we get the installer page
-            var installPage = _host.NavigateToInitialPage<InstallPage>();
+            var installPage = Host.NavigateToInitialPage<InstallPage>();
 
             installPage.Title.Should().Contain("Install Umbraco");
         }
-
-        [TearDown]
-        public void TearDown()
-        {
-            _host.Dispose();
-            WebServer.Stop();
-        }
-
+        
     }
 }
---
+++ src/Umbraco.Tests.Selenium/PageTests/SeleniumTestsBase.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using TestStack.Seleno.Configuration;
+using TestStack.Seleno.Configuration.WebServers;
+using Umbraco.Tests.Selenium.Helper;
+
+namespace Umbraco.Tests.Selenium.PageTests
+{
+    public class SeleniumTestsBase
+    {
+        protected SelenoHost Host;
+        protected static IisExpressWebServer WebServer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var app = new WebApplication(ProjectLocation.FromFolder(Constants.ApplicationNameForIISExpress), Config.Settings.PortNo);
+            WebServer = new IisExpressWebServer(app);
+            WebServer.Start();
+            Host = Selenium.Host.Instance;
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            Host.Dispose();
+            WebServer.Stop();
+        }
+    }
+}
